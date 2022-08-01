272668d58b0700e9080f1708667ed1bdbfbd29a3
Liang Chen <liang.chen@crypto.com>
2022-08-01 17:27:34 +0800

    Project setup

---
+++ hardhat.config.ts
@@ -0,0 +1,11 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+  gasReporter: {
+    enabled: true,
+  },
+};
+
+export default config;
