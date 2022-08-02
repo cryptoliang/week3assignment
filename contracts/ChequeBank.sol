37f180997bec2def58aa18773acadd0fff4eef88
Liang Chen <liang.chen@crypto.com>
2022-08-02 15:18:19 +0800

    Task 2: implement the withdraw function.

--- contracts/ChequeBank.sol
+++ contracts/ChequeBank.sol
@@ -1,8 +1,13 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 error ZeroAmount();
+error TransferFailed();
+error NotEnoughBalance(uint balance, uint need);
 
 contract ChequeBank {
+
     mapping(address => uint) public addressToBalance;
 
     struct ChequeInfo {
@@ -13,6 +18,7 @@ contract ChequeBank {
         address payee;
         address payer;
     }
+
     struct SignOverInfo {
         uint8 counter;
         bytes32 chequeId;
@@ -24,27 +30,42 @@ contract ChequeBank {
         ChequeInfo chequeInfo;
         bytes sig;
     }
+
     struct SignOver {
         SignOverInfo signOverInfo;
         bytes sig;
     }
 
     function deposit() payable external {
-        if(msg.value == 0) revert ZeroAmount();
+        if (msg.value == 0) revert ZeroAmount();
         addressToBalance[msg.sender] += msg.value;
     }
 
-    function withdraw(uint amount) external {}
+    function withdraw(uint amount) external {
+        if (amount == 0) revert ZeroAmount();
+        uint balance = addressToBalance[msg.sender];
+        if (amount > balance) revert NotEnoughBalance(balance, amount);
+
+        addressToBalance[msg.sender] = balance - amount;
+        (bool ok,) = msg.sender.call{value : amount}("");
+        if (!ok) revert TransferFailed();
+    }
+
     function withdrawTo(uint amount, address payable recipient) external {}
+
     function redeem(Cheque memory chequeData) external {}
+
     function revoke(bytes32 chequeId) external {}
+
     function notifySignOver(
         SignOver memory signOverData
     ) external {}
+
     function redeemSignOver(
         Cheque memory chequeData,
         SignOver[] memory signOverData
     ) external {}
+
     function isChequeValid(
         address payee,
         Cheque memory chequeData,
--- test/ChequeBank.test.ts
+++ test/ChequeBank.test.ts
@@ -13,15 +13,50 @@ describe("ChequeBank", function () {
     describe("deposit", function () {
         it("should deposit ethers", async function () {
             const {chequeBank, deployer} = await loadFixture(deployFixture);
-            let depositValue = ethers.utils.parseEther("1");
-            await chequeBank.deposit({value: depositValue});
-            expect(await chequeBank.addressToBalance(deployer.address)).equal(depositValue)
-            expect(await ethers.provider.getBalance(chequeBank.address)).equal(depositValue)
+            let depositAmount = ethers.utils.parseEther("1");
+            await chequeBank.deposit({value: depositAmount});
+            expect(await chequeBank.addressToBalance(deployer.address)).equal(depositAmount);
+            expect(await ethers.provider.getBalance(chequeBank.address)).equal(depositAmount);
         });
 
         it("should revert if deposit 0 ether", async function () {
             const {chequeBank} = await loadFixture(deployFixture);
             await expect(chequeBank.deposit()).to.be.revertedWithCustomError(chequeBank, "ZeroAmount");
+        });
+    });
+
+    describe("withdraw", function () {
+        it("should withdraw ethers", async function () {
+            const {chequeBank, deployer} = await loadFixture(deployFixture);
+            const depositAmount = ethers.utils.parseEther("1");
+            await chequeBank.deposit({value: depositAmount});
+
+            const withdrawAmount = ethers.utils.parseEther("0.8");
+            let deployerBeforeBalance = await ethers.provider.getBalance(deployer.address);
+
+            let tx = await chequeBank.withdraw(withdrawAmount);
+            let txReceipt = await tx.wait(1);
+
+            let transactionFee = txReceipt.gasUsed.mul(txReceipt.effectiveGasPrice);
+            let deployerAfterBalance = await ethers.provider.getBalance(deployer.address);
+
+            const leftAmount = depositAmount.sub(withdrawAmount);
+            expect(await ethers.provider.getBalance(chequeBank.address)).equal(leftAmount);
+            expect(await chequeBank.addressToBalance(deployer.address)).equal(leftAmount);
+            expect(deployerAfterBalance.add(transactionFee).sub(deployerBeforeBalance)).equal(withdrawAmount);
+        });
+
+        it("should revert if withdraw 0 ether", async function () {
+            const {chequeBank} = await loadFixture(deployFixture);
+            await expect(chequeBank.withdraw(0))
+                .to.be.revertedWithCustomError(chequeBank, "ZeroAmount");
+        });
+
+        it("should revert if withdraw more than account balance", async function () {
+            const {chequeBank} = await loadFixture(deployFixture);
+            await expect(chequeBank.withdraw(1))
+                .to.be.revertedWithCustomError(chequeBank, "NotEnoughBalance")
+                .withArgs(0, 1);
         });
     });
 })
